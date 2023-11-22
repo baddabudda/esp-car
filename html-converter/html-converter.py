5e258a6025d1f5cad0c146b22963f4be4b1a92a6
baddabudda <radkova.irene@gmail.com>
2023-11-23 00:03:19 +0300

    css added

---
+++ html-converter/html-converter.py
@@ -0,0 +1,22 @@
+filename = 'index.html'
+file = None
+string = 'extern const char * html = '
+
+with open(filename) as f:
+    for line in f:
+        line_processed = line.strip()
+        line_processed = line_processed.replace("\"", "\\\"")
+        string += "\"" + line_processed + " \"" + '\n'
+
+string += "\"\";"
+f.closed
+
+headerFilename = input('Enter header filename: ')
+headerFilename += '.h'
+headerPath = 'c:\\Users\\IR\\Documents\\Arduino\\esp-car\\monster-shield-car\\'
+
+with open(headerPath + headerFilename, 'w') as f:
+    f.write(string)
+f.closed
+
+# print(headerFilename)
